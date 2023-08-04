632e572dc0bc9ea4b37c137d596098ecd6abc8fb
Sathish <sathishu269@gmail.com>
2023-08-04 17:13:58 +0530

    Auto Push

---
+++ 02-escapesequence.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shebang Notation: This tells to the script with which interpreter this script has to be excuted.
+# There can only one Interpreter per script and it always first line and top of the script.
+
+
+echo Welcome to Bash Scripting
+
+# This is how we can execute the script
+#$ bash scriptname.sh     or   sh scriptname.sh    or      ./scriptname.sh 
+
+echo -e "line1\nline2"
+
+# Whenever your using special characters always enclose them in double quotes
