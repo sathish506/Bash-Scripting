8851da92ec9b9cd4147147a392619f9a18d1cbef
Sathish <sathishu269@gmail.com>
2023-08-04 16:14:10 +0530

    Shebang Notation

---
+++ 1-sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Shebang Notation: This tells to the script with which interpreter this script has to be excuted.
+# There can only one Interpreter per script and it always first line and top of the script.
+
+
+echo Welcome to Bash Scripting
+
+# This is how we can execute the script
+#$ bash scriptname.sh     or   sh scriptname.sh    or      ./scriptname.sh 
+
