4c1921fa96fdfd079282c8f4520c84c360ee8b8c
Sathish <sathishu269@gmail.com>
2023-08-04 17:25:45 +0530

    Auto Push

--- 02-escapesequence.sh
+++ 02-escapesequence.sh
@@ -9,6 +9,12 @@ echo Welcome to Bash Scripting
 # This is how we can execute the script
 #$ bash scriptname.sh     or   sh scriptname.sh    or      ./scriptname.sh 
 
+# \n : This indicates a new line
+# \t : This indicated a tab space
+
 echo -e "line1\nline2"
 
+echo -e "we\tare\tlearning\tcloud \nThis\tis\tmy\tTarget
+
 # Whenever your using special characters always enclose them in double quotes
+
