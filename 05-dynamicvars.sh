ea87ff214f594b0bae854fe3ae18d3220f98c1c5
Sathish <sathishu269@gmail.com>
2023-08-05 07:11:10 +0530

    Auto Push

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -5,3 +5,5 @@ echo "Todays date is $DATE"
 sessions_count=$(who |wc -l)
 
 echo "Total number of active sessions $sessions_count"
+
+echo -e "Total Number Of Active Sessions \e[32m $sessions_count\e[0m"
