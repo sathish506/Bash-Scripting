fbfcc2731aa97036554cae9213fb7415d33637a4
Sathish <sathishu269@gmail.com>
2023-08-06 14:55:38 +0530

    Auto Push

---
+++ 13-comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+<<COMMENT
+echo "Cloud DevOps Training"
+echo "Shall Scripting"
+a=100
+b=300
+echo $a
+COMMENT
+echo $b 
+
+
+# This is an example of Multi-line Comment, whatever we have enclosed in between <<COMMENT    COMMENT will be marked as comment
