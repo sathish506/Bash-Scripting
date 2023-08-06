8efcb8f564fde6028cf5daaebc1287c8f049d30c
Sathish <sathishu269@gmail.com>
2023-08-06 14:43:17 +0530

    Auto Push

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -9,3 +9,24 @@
 #     opt1)  commands-x ;; 
 #     opt2)  commands-y ;;
 # esac
+ACTION=$1
+
+case $ACTION in 
+    start)
+        echo -e "\e[32m Starting Payment Service \e[0m"
+        exit 0
+        ;;
+    stop)
+        echo -e "\e[31m Stopping Payment Service \e[0m"
+        exit 1
+        ;;
+    restart)
+        echo -e "\e[33mRestarting Payment Service \e[0m"
+        exit 2
+        ;;
+    *)
+        echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+        echo -e "\e[33m Example Usage \e[0m :\n \t bash scriptName stop"
+        exit 3
+        ;;
+esac 
