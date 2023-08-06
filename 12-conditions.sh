a27ea26e672a771ad74a345863bd4382ace5fd5d
Sathish <sathishu269@gmail.com>
2023-08-06 14:21:06 +0530

    Auto Push

---
+++ 12-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Conditons helps us to execute something only if SOME-Factor is true and if that factor is false then that something won't be executed.
+
+
+# Syntax Of CASE 
+# case $var in 
+#     opt1)  commands-x ;; 
+#     opt2)  commands-y ;;
+# esac
