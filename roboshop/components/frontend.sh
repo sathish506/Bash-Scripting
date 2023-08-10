75c3b495a65f4de4ac00f0a36918750e70a178d0
Sathish <sathishu269@gmail.com>
2023-08-10 17:02:44 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,36 +8,32 @@ if [ $USER_ID -ne 0 ] ; then
     exit 1
 fi
 
+stat() {
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32m success \e[0m"
+else
+     echo -e "\e[31m failure \e[0m"
+
+fi 
+}
+
+
 echo -e  "\e[35m configuring frontend .....! \e[0m \n"
 echo -n "Installing frontend :"
 yum install nginx -y &>> /tmp/frontend.log
+stat $?
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else
-  echo -e "\e[31m failure \e[0m"
-
-fi
 
 echo -n "starting nginx :"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx  &>> /tmp/frontend.log
+stat $?
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else
-  echo -e "\e[31m failure \e[0m"
-
-fi
 
 echo -n "Downloading the frontend component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-else
-  echo -e "\e[31m failure \e[0m"
 
-fi
 
 
