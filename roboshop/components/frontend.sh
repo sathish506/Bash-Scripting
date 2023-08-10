c34bcdea4136873c35fc2efe8a5b94aa6db10b4d
Sathish <sathishu269@gmail.com>
2023-08-10 16:24:32 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -12,4 +12,11 @@ echo -e  "\e[35m configuring frontend .....! \e[0m \n"
 echo "Installing frontend :"
 yum install nginx -y &>> /tmp/frontend.log
 
+if [$? -eq 0]; then
+  echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m failure \e[0m"
+fi
+
+
 
