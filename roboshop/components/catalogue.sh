4a6f58e6d5490d227f20ff7f05227cd25cc0e1e8
Sathish <sathishu269@gmail.com>
2023-08-11 22:07:44 +0530

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,55 @@
 #!/bin/bash
 
-echo "I am catalogue"
+# validate the user who is running the script is a root user or not.
+
+set -e 
+
+USER_ID=$(id -u)
+COMPONENT=catalogue
+LOGFILE=/tmp/COMPONENT.log
+Appuser="roboshop"
+
+if [ $USER_ID -ne 0 ] ; then    
+    echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ]; then
+    echo -e "\e[32m success \e[0m"
+else
+     echo -e "\e[31m failure \e[0m"
+     exit 2
+
+fi 
+}
+
+
+echo -e  "\e[35m configuring ${COMPONENT} .....! \e[0m \n"
+
+echo -n  "configuring ${COMPONENT} repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+stat $?
+
+echo -n "Installing ${COMPONENT}"
+yum install nodejs -y  &>> ${LOGFILE}
+stat $?
+
+echo -n " creating application user account : "
+useradd roboshop
+stat $?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
