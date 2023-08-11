8e18a3c77d9028c1c8f3cda5d035d8ac64146717
Sathish <sathishu269@gmail.com>
2023-08-11 16:32:20 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,6 +6,9 @@
 set -e 
 
 USER_ID=$(id -u)
+COMPONENT=frontend
+LOGFILE=/tmp/COMPONENT.log
+
 if [ $USER_ID -ne 0 ] ; then    
     echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
     exit 1
@@ -22,41 +25,41 @@ fi
 }
 
 
-echo -e  "\e[35m configuring frontend .....! \e[0m \n"
-echo -n "Installing frontend :"
-yum install nginx -y &>> /tmp/frontend.log
+echo -e  "\e[35m configuring COMPONENT .....! \e[0m \n"
+echo -n "Installing COMPONENT :"
+yum install nginx -y &>> LOGFILE
 stat $?
 
 
 echo -n "starting nginx :"
-systemctl enable nginx &>> /tmp/frontend.log
-systemctl start nginx  &>> /tmp/frontend.log
+systemctl enable nginx &>> LOGFILE
+systemctl start nginx  &>> LOGFILE
 stat $?
 
 
-echo -n "Downloading the frontend component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "Downloading the COMPONENT component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/COMPONENT/archive/main.zip"
 stat $?
 
-echo -n "cleanup of frontend"
+echo -n "cleanup of COMPONENT"
 cd /usr/share/nginx/html
-rm -rf *   &>>  /tmp/frontend.log
+rm -rf *   &>>  LOGFILE
 stat $?
 
-echo -n "extracting frontend"
-unzip /tmp/frontend.zip  &>> /tmp/frontend.log
+echo -n "extracting COMPONENT"
+unzip /tmp/COMPONENT.zip  &>> LOGFILE
 stat $?
 
-echo -n "sorting the frontend files"
-mv frontend-main/* .
+echo -n "sorting the COMPONENT files"
+mv COMPONENT-main/* .
 mv static/* .
-rm -rf frontend-main README.md  &>> /tmp/frontend.log
+rm -rf COMPONENT-main README.md  &>> LOGFILE
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo -n "Restarting the Frontend"
-systemctl daemone-reload  &>> /tmp/frontend.log
-systemctl restart nginx   &>> /tmp/frontend.log
+echo -n "Restarting the COMPONENT"
+systemctl daemone-reload  &>> LOGFILE
+systemctl restart nginx   &>> LOGFILE
 stat $?
 
 
