87ca2ea4389784cabf46456d35303618b505b4ce
Sathish <sathishu269@gmail.com>
2023-08-14 11:16:40 +0530

    Auto Push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -47,5 +47,9 @@ echo -n "Downloading the ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
-
+echo -n "copying the ${COMPONENT} to ${APPUSER} home directory"
+cd /home/${APPUSER}/
+rm -rf ${COMPONENT}   &>> ${LOGFILE}
+unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+stat $?
 
