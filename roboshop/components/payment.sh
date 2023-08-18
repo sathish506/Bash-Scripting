6958538be889b5875c954082e1851f2add7d26df
Sathish <sathishu269@gmail.com>
2023-08-18 17:50:59 +0530

    Auto Push

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -4,3 +4,6 @@ COMPONENT=payment
 
 # This is how we import the functions that are declared in a different file using source 
 source components/common.sh
+PYTHON                       # calling python function.
+
+echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
