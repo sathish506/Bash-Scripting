b616020f3d7b51f1f47faeeb00f61aa3678cc509
Sathish <sathishu269@gmail.com>
2023-08-18 12:16:45 +0530

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -18,3 +18,19 @@ echo -n "starting ${COMPONENT} :"
 systemctl enable mysqld     &>> ${LOGFILE}
 systemctl start mysqld      &>> ${LOGFILE}
 stat $?
+
+echo -n "Extracting the default mysql root password :"
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk -F " " '{print $NF}')
+stat $?
+
+# This should happen only once and that too for the first time, when it runs for the second time, jobs fails.
+# We need to ensure that this runs only once.
+
+echo "show databases;" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
+if [ $? -ne 0 ]; then 
+    echo -n "Performing default password reset of root account:"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
+    stat $?
+fi 
+
+
