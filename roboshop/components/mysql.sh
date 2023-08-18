75ff7e6b021fe924f90f2fd0832f4a84c87b0340
Sathish <sathishu269@gmail.com>
2023-08-18 12:20:13 +0530

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -33,4 +33,10 @@ if [ $? -ne 0 ]; then
     stat $?
 fi 
 
+echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password  &>>  ${LOGFILE}
+if [ $? -eq 0 ]; then 
+    echo -n "Uninstalling Password-validate plugin :"
+    echo "uninstall plugin validate_password" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
+    stat $?
+fi
 
