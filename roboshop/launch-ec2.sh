7bc9890420d28f8ab4bc214a1d3f5f3a1743a414
Sathish <sathishu269@gmail.com>
2023-08-21 12:07:06 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -12,11 +12,12 @@ if [ -z $1 ] ; then
 echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
 echo -e "\e[35m Ex Usage \e[0m \n \t \t $ bash launch-ec2.sh shipping"
 exit 1
+fi
 
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-052c68247aae56a95"         #B55-ALLOW ALL security group id
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID}\ 
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} \ 
 --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=${COMPONENT}}]" |jq '.Instances[].privateIpAddress')
 
 echo "private ip address of the $COMPONENT is $PRIVATEIP"
