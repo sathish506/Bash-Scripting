cc85b0186f8603af316998521468145797cf9839
Sathish <sathishu269@gmail.com>
2023-08-21 12:06:30 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -16,8 +16,9 @@ exit 1
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-052c68247aae56a95"         #B55-ALLOW ALL security group id
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} \ 
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID}\ 
 --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=${COMPONENT}}]" |jq '.Instances[].privateIpAddress')
+
 echo "private ip address of the $COMPONENT is $PRIVATEIP"
 
 
