1f6e962b31a2f808415181d7a3e8393ca6ce977b
Sathish <sathishu269@gmail.com>
2023-08-22 11:21:34 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -16,6 +16,7 @@ fi
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 # SG_ID="sg-052c68247aae56a95"         #B55-ALLOW ALL security group id
+# Hosted zone Id=Z099864834GFMARSYGETM
 
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b55-allow-all security group id
@@ -24,6 +25,12 @@ INSTANCE_TYPE="t3.micro"
 echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo "private ip address of the $COMPONENT is $PRIVATEIP"
+echo -e "Creating DNS Record of ${COMPONENT}: "
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+
+echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
+
 
 
 
