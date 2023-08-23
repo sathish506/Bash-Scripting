8ac62f6c54ffd7d3debd794566d640969cb90dff
Sathish <sathishu269@gmail.com>
2023-08-23 07:56:24 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -8,6 +8,9 @@
 # DNS record,Hosted Zone ID
 
 COMPONENT=$1
+Hosted zone Id=Z099864834GFMARSYGETM
+INSTANCE_TYPE="t3.micro"
+
 if [ -z $1 ] ; then
 echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
 echo -e "\e[35m Ex Usage \e[0m \n \t \t $ bash launch-ec2.sh shipping"
@@ -16,11 +19,12 @@ fi
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 # SG_ID="sg-052c68247aae56a95"         #B55-ALLOW ALL security group id
-# Hosted zone Id=Z099864834GFMARSYGETM
+
 
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b55-allow-all security group id
-INSTANCE_TYPE="t3.micro"
+
+create_ec2() {
 
 echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
@@ -30,6 +34,21 @@ sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route5
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 
 echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
+
+}
+
+if [ "$1" == "all" ]; then 
+
+    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmg; do 
+        COMPONENT=$component 
+        create_ec2
+    done 
+
+else 
+        create_ec2 
+fi
+
+
 
 
 
