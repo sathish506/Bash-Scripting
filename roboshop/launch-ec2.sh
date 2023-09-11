2835259d339ded98f78009b5e2cfce51923fe72f
Sathish <sathishu269@gmail.com>
2023-09-11 09:34:35 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,32 +1,41 @@
-#!/bin/bash 
+#!/bin/bash
 
-COMPONENT=$1 
+#To create an Instance what and all needed :
+# AMI ID
+# Type of Instance
+# Security group
+# How many instances you need
+# DNS record,Hosted Zone ID
+
+COMPONENT=$1
 ENV=$2
 HOSTEDZONEID="Z099864834GFMARSYGETM"
 INSTANCE_TYPE="t3.micro"
- 
+
 if [ -z $1 ] || [ -z $2 ]  ; then 
-    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
-    echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
-    exit 1
-fi 
+echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+echo -e "\e[35m Ex Usage \e[0m \n \t \t $ bash launch-ec2.sh shipping"
+exit 1
+fi
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
+# SG_ID="sg-052c68247aae56a95"         #B55-ALLOW ALL security group id
+
+
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
-SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=B55_ALLOW_ALL_SECURITY_GROUP | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=B55_ALLOW_ALL_SECURITY_GROUP | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b55-allow-all security group id
 
 create_ec2() {
 
-    echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
-    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
+echo -e "****** Creating \e[35m ${COMPONENT}-${ENV} \e[0m Server Is In Progress ************** "
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+echo "private ip address of the $COMPONENT is $PRIVATEIP"
+echo -e "Creating DNS Record of ${COMPONENT}: "
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 
-    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
-    echo -e "Creating DNS Record of ${COMPONENT}: "
+echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
 
-    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
-
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
 }
 
 if [ "$1" == "all" ]; then 
@@ -38,4 +47,4 @@ if [ "$1" == "all" ]; then
 
 else 
         create_ec2 
-fi 
+fi
