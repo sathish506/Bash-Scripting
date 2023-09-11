ea2ad617f30175dac10da6235bcea8b797f8d15c
Sathish <sathishu269@gmail.com>
2023-09-11 09:42:41 +0530

    Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -23,13 +23,13 @@ fi
 
 
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
-SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=B55_ALLOW_ALL | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b55-allow-all security group id
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=B55_ALLOW_ALL_SECURITY_GROUP | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b55-allow-all security group id
 
 create_ec2() {
 
 echo -e "****** Creating \e[35m ${COMPONENT}-${ENV} \e[0m Server Is In Progress ************** "
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-echo "private ip address of the $COMPONENT is $PRIVATEIP"
+echo "private ip address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
 echo -e "Creating DNS Record of ${COMPONENT}: "
 sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
